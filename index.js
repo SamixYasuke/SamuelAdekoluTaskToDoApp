df9a8f7b6624014e63a380d8e3f8fd0138abb3a6
Samuel Adekolu <95057238+SamixYasuke@users.noreply.github.com>
2023-05-21 23:26:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const completedBtn = Array.from(document.querySelectorAll(".todo-list-content ul
 const todoInput = document.getElementById("todo-input");
 const addTodoBtn = document.getElementById("addTodo");
 const todolistContainer = document.querySelector(".todo-list-content ul");
+const removeListBtn = document.querySelectorAll(".todo-list-content ul li img");
 
 completedBtn.forEach((btn)=>{
     btn.addEventListener("click", (e)=>{
@@ -18,19 +19,62 @@ addTodoBtn.addEventListener("click", () => {
       alert("The Input Can't be empty");
       addTodoBtn.checked = false;
     } else {
-        let li = document.createElement("li");
-        var checkBoxInput = document.createElement("input");
-        var h4 = document.createElement("h4");
-        var label = document.createElement("label");
-        checkBoxInput.setAttribute("type", "checkbox");
-        h4.textContent = input;
-        li.appendChild(checkBoxInput);
-        li.appendChild(label);
-        li.appendChild(h4);
-        todolistContainer.appendChild(li);
+      var li = document.createElement("li");
+      var checkBoxInput = document.createElement("input");
+      var p = document.createElement("p");
+      var label = document.createElement("label");
+      var image = document.createElement("img");
+      image.setAttribute("src", "images/icon-cross.svg");
+      checkBoxInput.setAttribute("type", "checkbox");
+      p.textContent = input;
+      li.appendChild(checkBoxInput);
+      li.appendChild(label);
+      li.appendChild(p);
+      li.appendChild(image)
+      todolistContainer.appendChild(li);
+      saveTodoListItems();
     }
     setTimeout(() => {
-      todoInput.value = ""; // Corrected variable name
+      todoInput.value = "";
       addTodoBtn.checked = false;
     }, 1000);
 });
+
+
+removeListBtn.forEach((btn)=>{
+    btn.addEventListener("click", (e)=>{
+        e.target.parentElement.remove();
+        saveTodoListItems();
+    });
+})
+  
+function saveTodoListItems() {
+    const todoItems = Array.from(todolistContainer.querySelectorAll("li")).map((li) => {
+      return {
+        checked: li.querySelector("input").checked,
+        text: li.querySelector("p").textContent
+      };
+    });
+    localStorage.setItem("todoListItems", JSON.stringify(todoItems));
+}
+  
+function getLists() {
+    const todoItems = JSON.parse(localStorage.getItem("todoListItems"));
+    if (todoItems && todoItems.length > 0) {
+      todoItems.forEach((item) => {
+        var li = document.createElement("li");
+        var checkBoxInput = document.createElement("input");
+        var p = document.createElement("p");
+        var label = document.createElement("label");
+        checkBoxInput.setAttribute("type", "checkbox");
+        checkBoxInput.checked = item.checked;
+        p.textContent = item.text;
+        li.appendChild(checkBoxInput);
+        li.appendChild(label);
+        li.appendChild(p);
+        todolistContainer.appendChild(li);
+      });
+    }
+}
+  
+getLists();
